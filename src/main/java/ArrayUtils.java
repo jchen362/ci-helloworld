8e60e70a5fc47759c9d1fcc2bff303157c9a6ccc
Jianwei Chen <98350779+jchen362@users.noreply.github.com>
2025-03-31 12:15:06 -0400

    a

--- src/main/java/ArrayUtils.java
+++ src/main/java/ArrayUtils.java
@@ -30,10 +30,9 @@ public class ArrayUtils {
     int count = 0;
     for (int i = 0; i < x.length; i++) {
       if (x[i] % 2 != 0 || x[i] > 0) {
-        count++;
+        count+=1;
       }
     }
     return count;
   }
-
 }
